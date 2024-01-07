2cf0d103220893413aa85d8627bb5b2490ae0df6
Maybe Ange <111668398+MaybeAnge@users.noreply.github.com>
2024-01-08 00:36:08 +0100

    Add files via upload

---
+++ admin/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/assets/images/favicon.png">
+    <link rel="stylesheet" href="/css/login.css">
+    <title>Login Admins</title>
+</head>
+<body>
+
+    <div class="container-outer">
+        <div class="container-inner">
+            <img src="/assets/images/user-profil.png" alt="">
+            <h1>Login Admin</h1>
+            <input type="username" placeholder="Username">
+            <input type="email" placeholder="E-mail Address">
+            <input type="password" placeholder="Password">
+            <button>Login Now</button>
+            <a href="https://maybe-ange.com">Powered By Maybe Ange™ Corporation</a>
+        </div>
+    </div>
+    
+</body>
+</html>
